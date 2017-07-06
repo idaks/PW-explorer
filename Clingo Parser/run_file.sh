da277498437523cb8292c225605e700dda638ee1
Sahil Gupta <sjgupta2@illinois.edu>
2017-07-06 09:14:30 -0500

    minor change to shell script

--- Clingo Parser/run_file.sh
+++ Clingo Parser/run_file.sh
@@ -3,24 +3,17 @@
 #clingo_input_fname = $1
 #project_name = $2
 
-cp $1 /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_input/$2.lp4
+cp $1 Mini\ Workflow/clingo_input/$2.lp4
 
 echo "Copying Done"
 
-# if [[-z $3]]; 
-# then
-# 	clingo -n $3 /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_input/$2.lp4 > /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_output/$2.txt
-# else
-# 	clingo -n 0 /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_input/$2.lp4 > /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_output/$2.txt
-# fi
-
-clingo -n ${3:-0} -W none /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_input/$2.lp4 > /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_output/$2.txt
+clingo -n ${3:-0} -W none Mini\ Workflow/clingo_input/$2.lp4 > Mini\ Workflow/clingo_output/$2.txt
 
 echo "Clingo Ouput Done"
 
-python Preprocessing.py /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_output/$2.txt
+python Preprocessing.py Mini\ Workflow/clingo_output/$2.txt
 
 echo "Preprocessing Done"
 
-python AntlrClingo.py /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/clingo_output/$2.txt 2 > /Users/sahil1105/PW-explorer/Clingo\ Parser/Mini\ Workflow/parser_output/$2.txt
+python AntlrClingo.py Mini\ Workflow/clingo_output/$2.txt 2 > Mini\ Workflow/parser_output/$2.txt
 
