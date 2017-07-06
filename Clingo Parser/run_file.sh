b8406bb1e841d02363676cc1f0ab845aef3c41f4
Sahil Gupta <sjgupta2@illinois.edu>
2017-07-06 10:54:55 -0500

    minor change to run_file.sh

--- Clingo Parser/run_file.sh
+++ Clingo Parser/run_file.sh
@@ -15,7 +15,7 @@ python Preprocessing.py Mini\ Workflow/clingo_output/$2.txt
 
 echo "Preprocessing Done"
 
-echo "Enter a comma-separated list of formats you want to export the project" $2 "in. Options: sql, csv, h5, msg, pkl"
+echo "Enter a comma-separated list of formats you want to export the project" $2 "in. Options: sql, csv, h5, msg, pkl. Hit return to not export in any format."
 
 python AntlrClingo.py Mini\ Workflow/clingo_output/$2.txt $2 > Mini\ Workflow/parser_output/$2.txt
 
