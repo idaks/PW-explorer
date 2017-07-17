0183804d2b353c6d0b54c9b54057e24e868a5e7f
Sahil Gupta <sjgupta2@illinois.edu>
2017-07-17 12:14:28 -0500

    implemented dbscan clustering and plotting

--- Clingo Parser/AntlrClingo.py
+++ Clingo Parser/AntlrClingo.py
@@ -11,6 +11,9 @@ from antlr4.tree.Trees import Trees
 import sqlite3
 import errno    
 import os
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
 
 ###################################################################
 
@@ -1060,7 +1063,6 @@ def unique_tuples_panda(rl_id = 0, col_names = [], pws_to_consider = [j for j in
 	return unique_tuples
 
 
-
 ###########################################################################################
 
 #prototype dist function:
@@ -1148,14 +1150,53 @@ def dist(pw_id_1, pw_id_2):
 
 ###########################################################################################
 
-# dist_matrix = np.zeros((len(pws),len(pws)))
-# for i in range(1, len(pws)+1):
-# 	for j in range(i+1, len(pws)+1):
-# 		dist_matrix[i-1, j-1] = dist_matrix[j-1,i-1] = dist(i,j)
-# 		print 'Distance between PWs', i, 'and', j, 'is', dist_matrix[i-1,j-1]
+def clustering(dist_matrix):
+	db = DBSCAN(metric = 'precomputed')#, eps = 0.3, min_samples = 1)
+	labels = db.fit_predict(dist_matrix)
+	print labels
 
 
-# print dist_matrix
+	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+	core_samples_mask[db.core_sample_indices_] = True
+	labels = db.labels_
+
+	# Number of clusters in labels, ignoring noise if present.
+	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+	unique_labels = set(labels)
+	colors = [plt.cm.Spectral(each)
+	          for each in np.linspace(0, 1, len(unique_labels))]
+	for k, col in zip(unique_labels, colors):
+	    if k == -1:
+	        # Black used for noise.
+	        col = [0, 0, 0, 1]
+
+	    class_member_mask = (labels == k)
+
+	    xy = dist_matrix[class_member_mask & core_samples_mask]
+	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+	             markeredgecolor='k', markersize=14)
+
+	    xy = dist_matrix[class_member_mask & ~core_samples_mask]
+	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+	             markeredgecolor='k', markersize=6)
+
+	plt.title('Estimated number of clusters: %d' % n_clusters_)
+	plt.show()
+
+
+dist_matrix = np.zeros((len(pws),len(pws)))
+for i in range(1, len(pws)+1):
+	for j in range(i+1, len(pws)+1):
+		dist_matrix[i-1, j-1] = dist_matrix[j-1,i-1] = dist(i,j)
+		#print 'Distance between PWs', i, 'and', j, 'is', dist_matrix[i-1,j-1]
+
+dist_matrix = (dist_matrix - np.min(dist_matrix))/(np.max(dist_matrix) - np.min(dist_matrix))
+print dist_matrix
+
+clustering(dist_matrix)
+
+
+
 
 
 
