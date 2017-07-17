d46e24f3dcca3f1c2f720ebf9830367a4dbe21f4
Qiwen Wang <qwang70@illinois.edu>
2017-07-17 05:12:21 -0500

    nqueens

--- Dlv Parser/dlv_parser/DlvParser.py
+++ Dlv Parser/dlv_parser/DlvParser.py
@@ -8,19 +8,22 @@ import sys
 def serializedATN():
     with StringIO() as buf:
         buf.write(u"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3")
-        buf.write(u"\t*\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\3\2\3\2\3")
-        buf.write(u"\2\3\2\7\2\21\n\2\f\2\16\2\24\13\2\3\2\3\2\3\3\3\3\3")
-        buf.write(u"\3\3\4\3\4\3\4\3\4\7\4\37\n\4\f\4\16\4\"\13\4\3\4\3\4")
-        buf.write(u"\3\5\3\5\3\6\3\6\3\6\2\2\7\2\4\6\b\n\2\2\2&\2\f\3\2\2")
-        buf.write(u"\2\4\27\3\2\2\2\6\32\3\2\2\2\b%\3\2\2\2\n\'\3\2\2\2\f")
-        buf.write(u"\r\7\3\2\2\r\22\5\4\3\2\16\17\7\4\2\2\17\21\5\4\3\2\20")
-        buf.write(u"\16\3\2\2\2\21\24\3\2\2\2\22\20\3\2\2\2\22\23\3\2\2\2")
-        buf.write(u"\23\25\3\2\2\2\24\22\3\2\2\2\25\26\7\5\2\2\26\3\3\2\2")
-        buf.write(u"\2\27\30\5\n\6\2\30\31\5\6\4\2\31\5\3\2\2\2\32\33\7\6")
-        buf.write(u"\2\2\33 \5\b\5\2\34\35\7\4\2\2\35\37\5\b\5\2\36\34\3")
-        buf.write(u"\2\2\2\37\"\3\2\2\2 \36\3\2\2\2 !\3\2\2\2!#\3\2\2\2\"")
-        buf.write(u" \3\2\2\2#$\7\7\2\2$\7\3\2\2\2%&\7\b\2\2&\t\3\2\2\2\'")
-        buf.write(u"(\7\b\2\2(\13\3\2\2\2\4\22 ")
+        buf.write(u"\t\63\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write(u"\3\2\7\2\20\n\2\f\2\16\2\23\13\2\3\3\3\3\3\3\3\3\3\4")
+        buf.write(u"\3\4\3\4\3\4\3\4\3\4\7\4\37\n\4\f\4\16\4\"\13\4\3\5\3")
+        buf.write(u"\5\3\5\3\5\7\5(\n\5\f\5\16\5+\13\5\3\5\3\5\3\6\3\6\3")
+        buf.write(u"\7\3\7\3\7\2\2\b\2\4\6\b\n\f\2\2\2/\2\21\3\2\2\2\4\24")
+        buf.write(u"\3\2\2\2\6\30\3\2\2\2\b#\3\2\2\2\n.\3\2\2\2\f\60\3\2")
+        buf.write(u"\2\2\16\20\5\4\3\2\17\16\3\2\2\2\20\23\3\2\2\2\21\17")
+        buf.write(u"\3\2\2\2\21\22\3\2\2\2\22\3\3\2\2\2\23\21\3\2\2\2\24")
+        buf.write(u"\25\7\3\2\2\25\26\5\6\4\2\26\27\7\4\2\2\27\5\3\2\2\2")
+        buf.write(u"\30\31\5\f\7\2\31 \5\b\5\2\32\33\7\5\2\2\33\34\5\f\7")
+        buf.write(u"\2\34\35\5\b\5\2\35\37\3\2\2\2\36\32\3\2\2\2\37\"\3\2")
+        buf.write(u"\2\2 \36\3\2\2\2 !\3\2\2\2!\7\3\2\2\2\" \3\2\2\2#$\7")
+        buf.write(u"\6\2\2$)\5\n\6\2%&\7\5\2\2&(\5\n\6\2\'%\3\2\2\2(+\3\2")
+        buf.write(u"\2\2)\'\3\2\2\2)*\3\2\2\2*,\3\2\2\2+)\3\2\2\2,-\7\7\2")
+        buf.write(u"\2-\t\3\2\2\2./\7\b\2\2/\13\3\2\2\2\60\61\7\b\2\2\61")
+        buf.write(u"\r\3\2\2\2\5\21 )")
         return buf.getvalue()
 
 
@@ -34,18 +37,20 @@ class DlvParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [ u"<INVALID>", u"'{'", u"','", u"'}'", u"'('", u"')'" ]
+    literalNames = [ u"<INVALID>", u"'{'", u"'}'", u"','", u"'('", u"')'" ]
 
     symbolicNames = [ u"<INVALID>", u"<INVALID>", u"<INVALID>", u"<INVALID>", 
                       u"<INVALID>", u"<INVALID>", u"TEXT", u"WS" ]
 
-    RULE_solution = 0
-    RULE_atoms = 1
-    RULE_atom_vals = 2
-    RULE_val = 3
-    RULE_atom = 4
+    RULE_dlvOutput = 0
+    RULE_solution = 1
+    RULE_atoms = 2
+    RULE_atom_vals = 3
+    RULE_val = 4
+    RULE_atom = 5
 
-    ruleNames =  [ u"solution", u"atoms", u"atom_vals", u"val", u"atom" ]
+    ruleNames =  [ u"dlvOutput", u"solution", u"atoms", u"atom_vals", u"val", 
+                   u"atom" ]
 
     EOF = Token.EOF
     T__0=1
@@ -64,17 +69,66 @@ class DlvParser ( Parser ):
 
 
 
+    class DlvOutputContext(ParserRuleContext):
+
+        def __init__(self, parser, parent=None, invokingState=-1):
+            super(DlvParser.DlvOutputContext, self).__init__(parent, invokingState)
+            self.parser = parser
+
+        def solution(self, i=None):
+            if i is None:
+                return self.getTypedRuleContexts(DlvParser.SolutionContext)
+            else:
+                return self.getTypedRuleContext(DlvParser.SolutionContext,i)
+
+
+        def getRuleIndex(self):
+            return DlvParser.RULE_dlvOutput
+
+        def enterRule(self, listener):
+            if hasattr(listener, "enterDlvOutput"):
+                listener.enterDlvOutput(self)
+
+        def exitRule(self, listener):
+            if hasattr(listener, "exitDlvOutput"):
+                listener.exitDlvOutput(self)
+
+
+
+
+    def dlvOutput(self):
+
+        localctx = DlvParser.DlvOutputContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 0, self.RULE_dlvOutput)
+        self._la = 0 # Token type
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 15
+            self._errHandler.sync(self)
+            _la = self._input.LA(1)
+            while _la==DlvParser.T__0:
+                self.state = 12
+                self.solution()
+                self.state = 17
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
     class SolutionContext(ParserRuleContext):
 
         def __init__(self, parser, parent=None, invokingState=-1):
             super(DlvParser.SolutionContext, self).__init__(parent, invokingState)
             self.parser = parser
 
-        def atoms(self, i=None):
-            if i is None:
-                return self.getTypedRuleContexts(DlvParser.AtomsContext)
-            else:
-                return self.getTypedRuleContext(DlvParser.AtomsContext,i)
+        def atoms(self):
+            return self.getTypedRuleContext(DlvParser.AtomsContext,0)
 
 
         def getRuleIndex(self):
@@ -94,28 +148,15 @@ class DlvParser ( Parser ):
     def solution(self):
 
         localctx = DlvParser.SolutionContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 0, self.RULE_solution)
-        self._la = 0 # Token type
+        self.enterRule(localctx, 2, self.RULE_solution)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 10
+            self.state = 18
             self.match(DlvParser.T__0)
-            self.state = 11
-            self.atoms()
-            self.state = 16
-            self._errHandler.sync(self)
-            _la = self._input.LA(1)
-            while _la==DlvParser.T__1:
-                self.state = 12
-                self.match(DlvParser.T__1)
-                self.state = 13
-                self.atoms()
-                self.state = 18
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-
             self.state = 19
-            self.match(DlvParser.T__2)
+            self.atoms()
+            self.state = 20
+            self.match(DlvParser.T__1)
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -130,12 +171,18 @@ class DlvParser ( Parser ):
             super(DlvParser.AtomsContext, self).__init__(parent, invokingState)
             self.parser = parser
 
-        def atom(self):
-            return self.getTypedRuleContext(DlvParser.AtomContext,0)
+        def atom(self, i=None):
+            if i is None:
+                return self.getTypedRuleContexts(DlvParser.AtomContext)
+            else:
+                return self.getTypedRuleContext(DlvParser.AtomContext,i)
 
 
-        def atom_vals(self):
-            return self.getTypedRuleContext(DlvParser.Atom_valsContext,0)
+        def atom_vals(self, i=None):
+            if i is None:
+                return self.getTypedRuleContexts(DlvParser.Atom_valsContext)
+            else:
+                return self.getTypedRuleContext(DlvParser.Atom_valsContext,i)
 
 
         def getRuleIndex(self):
@@ -155,13 +202,28 @@ class DlvParser ( Parser ):
     def atoms(self):
 
         localctx = DlvParser.AtomsContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 2, self.RULE_atoms)
+        self.enterRule(localctx, 4, self.RULE_atoms)
+        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 21
-            self.atom()
             self.state = 22
+            self.atom()
+            self.state = 23
             self.atom_vals()
+            self.state = 30
+            self._errHandler.sync(self)
+            _la = self._input.LA(1)
+            while _la==DlvParser.T__2:
+                self.state = 24
+                self.match(DlvParser.T__2)
+                self.state = 25
+                self.atom()
+                self.state = 26
+                self.atom_vals()
+                self.state = 32
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
@@ -200,27 +262,27 @@ class DlvParser ( Parser ):
     def atom_vals(self):
 
         localctx = DlvParser.Atom_valsContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 4, self.RULE_atom_vals)
+        self.enterRule(localctx, 6, self.RULE_atom_vals)
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 24
+            self.state = 33
             self.match(DlvParser.T__3)
-            self.state = 25
+            self.state = 34
             self.val()
-            self.state = 30
+            self.state = 39
             self._errHandler.sync(self)
             _la = self._input.LA(1)
-            while _la==DlvParser.T__1:
-                self.state = 26
-                self.match(DlvParser.T__1)
-                self.state = 27
+            while _la==DlvParser.T__2:
+                self.state = 35
+                self.match(DlvParser.T__2)
+                self.state = 36
                 self.val()
-                self.state = 32
+                self.state = 41
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
-            self.state = 33
+            self.state = 42
             self.match(DlvParser.T__4)
         except RecognitionException as re:
             localctx.exception = re
@@ -256,10 +318,10 @@ class DlvParser ( Parser ):
     def val(self):
 
         localctx = DlvParser.ValContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 6, self.RULE_val)
+        self.enterRule(localctx, 8, self.RULE_val)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 35
+            self.state = 44
             self.match(DlvParser.TEXT)
         except RecognitionException as re:
             localctx.exception = re
@@ -295,10 +357,10 @@ class DlvParser ( Parser ):
     def atom(self):
 
         localctx = DlvParser.AtomContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 8, self.RULE_atom)
+        self.enterRule(localctx, 10, self.RULE_atom)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 37
+            self.state = 46
             self.match(DlvParser.TEXT)
         except RecognitionException as re:
             localctx.exception = re
