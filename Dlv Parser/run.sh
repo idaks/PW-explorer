cf1810f993f1228501b79da227b323f658458bae
Qiwen Wang <qwang70@illinois.edu>
2017-08-04 00:30:00 -0500

    4-coloring

--- Dlv Parser/run.sh
+++ Dlv Parser/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 DLV=./dlv_bin/dlv.i386-apple-darwin.bin
+
+# 8-queen
 # output ASP file
 $DLV -silent -N=8 Mini\ Workflow/dlv_input/nqueens.dlv > Mini\ Workflow/dlv_output/nqueens.asp
 # parse the ASP file and output files in other common format
 python dlv_parser/AntlrDlv.py Mini\ Workflow/dlv_output/nqueens.asp --sql --csv --hdf --msg --pkl
+
+# 4-coloring
+# output ASP file
+$DLV -silent Mini\ Workflow/dlv_input/coloring.dlv -filter=color > Mini\ Workflow/dlv_output/coloring.asp
+# parse the ASP file and output files in other common format
+python dlv_parser/AntlrDlv.py Mini\ Workflow/dlv_output/coloring.asp --sql --csv  --msg --pkl
