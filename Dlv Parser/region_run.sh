73de5ffec9e2e737a6c11297a79ad7b17b8bd652
Qiwen Wang <qwang70@illinois.edu>
2017-08-30 07:13:20 -0500

    add queries

--- Dlv Parser/region_run.sh
+++ Dlv Parser/region_run.sh
@@ -4,14 +4,19 @@ DLV=./dlv_bin/dlv.i386-apple-darwin.bin
 echo "region_switch"
 # list of project names of CEN-NDC-regions with exactly one restriction being removed
 PROJECT_NAMES=(2017-05-31-16:47:24-CEN-NDC-regions 2017-05-31-16:47:53-CEN-NDC-regions 2017-05-31-16:49:19-CEN-NDC-regions 2017-05-31-16:50:20-CEN-NDC-regions 2017-05-31-16:51:01-CEN-NDC-regions 2017-05-31-16:51:38-CEN-NDC-regions 2017-05-31-16:52:17-CEN-NDC-regions 2017-05-31-16:53:02-CEN-NDC-regions)
+QUERY_FOLDER="../iConference/sqlQuery"
 for PROJECT_NAME in ${PROJECT_NAMES[*]}
 do
     echo $PROJECT_NAME
-    echo "#possible world, #overlapped regions"
     # output ASP file
-    TEST_FOLDER="../iConference/$PROJECT_NAME/1-ASP-input-code"
-    $DLV -silent $TEST_FOLDER/CEN-NDC-regions_pw.dlv $TEST_FOLDER/CEN-NDC-regions_ixswitch.dlv $TEST_FOLDER/CEN-NDC-regions_pwswitch.dlv -filter=rel > Mini\ Workflow/dlv_output/$PROJECT_NAME.asp
+    PROJECT_FOLDER="../iConference/$PROJECT_NAME/1-ASP-input-code"
+    $DLV -silent $PROJECT_FOLDER/CEN-NDC-regions_pw.dlv $PROJECT_FOLDER/CEN-NDC-regions_ixswitch.dlv $PROJECT_FOLDER/CEN-NDC-regions_pwswitch.dlv -filter=rel > Mini\ Workflow/dlv_output/$PROJECT_NAME.asp
     # parse the ASP file and output files in other common format
     dlv_parser/pwe export Mini\ Workflow/dlv_output/$PROJECT_NAME.asp -sql -csv
-    dlv_parser/pwe sqlQuery Mini\ Workflow/parser_output/sql_exports/$PROJECT_NAME/$PROJECT_NAME.db -f dlv_parser/test.sql
+    echo "#possible world, #overlapped regions"
+    dlv_parser/pwe sqlQuery Mini\ Workflow/parser_output/sql_exports/$PROJECT_NAME/$PROJECT_NAME.db -f $QUERY_FOLDER/overlap.sql
+    echo "#possible world, #subset regions"
+    dlv_parser/pwe sqlQuery Mini\ Workflow/parser_output/sql_exports/$PROJECT_NAME/$PROJECT_NAME.db -f $QUERY_FOLDER/subset.sql
+    echo "#possible world, #all not equal relations"
+    dlv_parser/pwe sqlQuery Mini\ Workflow/parser_output/sql_exports/$PROJECT_NAME/$PROJECT_NAME.db -f $QUERY_FOLDER/allNotEqualRelationCount.sql
 done
