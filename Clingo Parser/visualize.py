dea158c1cfbf8366507dead4c90bc9a37d82e660
Sahil Gupta <sahil1105@hotmail.com>
2017-11-13 13:24:46 -0600

    added custom visualisation functionality and tried the mag 7 example

--- Clingo Parser/visualize.py
+++ Clingo Parser/visualize.py
@@ -30,6 +30,7 @@ parser.add_argument("-p", "--project_name", type = str, help = "provide session/
 parser.add_argument("-mds", action = 'store_true', default = False, help = "produce a Multidimensional Scaling Graph Output using the Neato Program")
 parser.add_argument("-clustering", action = 'store_true', default = False, help = "use DBScan Algorithm to cluster the Possible Worlds")
 parser.add_argument("-dendrogram", action = 'store_true', default = False, help = "create various dendrograms using scipy")
+parser.add_argument("-custom_visualisation_func", type = str, help = "provide the .py file (without the .py) containing your custom visualisation function. The function signature should be visualize(dfs = None, pws = None, relations = None, conn = None) where the four arguments refer to the data acquired from parsing the ASP solutions and the connection to the generated sqlite database respectively. The function should create the visualization and may or may not return anything. Ensure that the file is in the same directory as this script. You can use the functions in sql_funcs.py to design these visualisation functions")
 args = parser.parse_args()
 
 project_name = ''
@@ -337,9 +338,17 @@ if len(pws) > 1:
 		linkage_dendrogram(dist_matrix)
 	#dendrogram_plotly(np.array([i for i in range(len(pws))]))
 
+if args.custom_visualisation_func:
 
+	try:
+		a=importlib.import_module(args.custom_visualisation_func)
+		visualisation_func = a.visualize
+	except Exception, e:
+		print "Error importing from the given file"
+		print "Error: ", str(e)
+		exit(1)
 
-
+	visualisation_func(dfs, pws, relations, conn)
 
 
 
