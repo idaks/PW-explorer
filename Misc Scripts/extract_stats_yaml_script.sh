2d5a3b667aa19129132897dd72a77684505c3c3d
sjgupta2 <sjgupta2@illinois.edu>
2018-09-21 12:20:19 -0500

    removing unnecessary files

--- Misc Scripts/extract_stats_yaml_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROJECT_NAMES=(2017-09-11-20:24:04-figure4C_minart_0_1PWmncb 2017-09-11-20:25:17-figure4C_minart_0_25PWmncb 2017-09-11-20:26:09-figure4C_minart_0_7PWmncb 2017-09-11-20:27:05-figure4C_minart_0_25PWmncb 2017-09-11-20:28:29-figure4C_minart_0_25PWmncb)
-for PROJECT_NAME in ${PROJECT_NAMES[*]}
-do
-	PROJECT_FOLDER=Andropogon-ZOOM_IN/$PROJECT_NAME
-	python3 extract_stats_from_yaml.py "$PROJECT_FOLDER" "figure4C_minart_0"
-done
