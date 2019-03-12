f2d31b08f710a37230469e22e43247f80e8bd880
sjgupta2 <sjgupta2@illinois.edu>
2019-03-11 21:49:29 -0500

    releasing v0.0.19 with prolog syntax highlighting for jupyter nbs

---
+++ PW_explorer/pwe_nb_helper.py
@@ -0,0 +1,31 @@
+import os
+from pygments.lexers import PrologLexer
+from pygments.formatters import HtmlFormatter
+from IPython.core.display import HTML
+from IPython.display import display
+from pygments import highlight
+
+
+## Helper functions meant for Jupyter Notebooks.
+
+class ASPRules(str):
+
+    def __new__(self, descriptor):
+        if os.path.isfile(descriptor):
+            with open(descriptor, 'r') as f:
+                tmp_str = f.read()
+        else:
+            tmp_str = descriptor
+        tmp_str = tmp_str.strip()
+        return super(ASPRules, self).__new__(self, tmp_str)
+
+    def _repr_html_(self):
+        display(HTML("""
+        <style>
+        {pygments_css}
+        </style>
+        """.format(pygments_css=HtmlFormatter().get_style_defs('.highlight'))))
+        if len(self.splitlines()) > 1:
+            return HTML(data=highlight(self, PrologLexer(), HtmlFormatter(linenos='inline')))._repr_html_()
+        else:
+            return HTML(data=highlight(self, PrologLexer(), HtmlFormatter()))._repr_html_()
