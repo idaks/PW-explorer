d074cb854129316e590518887c2eb15c7f922ba7
sjgupta2 <sjgupta2@illinois.edu>
2019-05-29 10:39:36 -0500

    v0.0.25 with export module updated to handle recursive facts

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name="PW_explorer",
-    version="0.0.24",
+    version="0.0.25",
     author="Sahil Gupta",
     author_email="",
     description="An Extensible Possible World Explorer for Answer Set Programming",
